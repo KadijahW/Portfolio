d53fbeb216aca36c6cb481f1d55a1f13a588aad5
kadjiah <kadijahwilson@pursuit.org>
2020-03-08 17:05:10 -0400

    added CSS to landing page and a logo

--- my-portfolio/src/Componenets/Home.jsx
+++ my-portfolio/src/Componenets/Home.jsx
@@ -1,1 +1,9 @@
 import React from 'react'
+
+const Home = () => {
+    return(
+        <h1>HEYYYYYY</h1>
+    )
+}
+
+export default Home;
--- my-portfolio/src/Componenets/Nav.jsx
+++ my-portfolio/src/Componenets/Nav.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Home from './Home'
+import '../CSS/Nav.css'
 
 const Nav = () => {
-return (
-    <nav>
-        <span className="logo">KW</span>{" "}
-        <Link to="/">Home</Link>{" "}
-        <Link to="/about">About</Link>{" "}
-        <Link to="/about">Projects</Link>{" "}
-        <Link to="/about">Resume</Link>{" "}
-
-
-
-    </nav>
+    return (
+        <div className='container'>
+        <nav>
+             <div className='profile'>
+             <img className='profile-logo' src={require('../Profile-Logo.png')} alt='profile-logo'/>
+             </div>
+         <div className='links'>
+            <Link to="/about">About</Link>{" "}
+            <Link to="/projects">Projects</Link>{" "}
+            <Link to="/">Resume</Link>{" "}
+            </div>
+        </nav>
+        </div>
 )
 }
 
