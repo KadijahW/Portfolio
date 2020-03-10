0df5c34208aabdc6b6a2ac75217ca86fea1e72aa
kadjiah <kadijahwilson@pursuit.org>
2020-03-09 20:59:17 -0400

    used react typical to add the type writer effect

--- my-portfolio/src/Componenets/Home.jsx
+++ my-portfolio/src/Componenets/Home.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
-
+import Typical from 'react-typical'
 
 const Home = () => {
-
-
-
 
     return (
         <div>
             <div className='intro'>
-            <h1> </h1>
+                <h1>Hi.</h1>
+                <p>
+                    <Typical
+                        loop={Infinity}
+                        wrapper="b"
+                        steps={[
+                            "My name is Kadijah.",
+                            2000,
+                            "I'm a Software Engineer.",
+                            2000,
+                            "I'm based in Brooklyn, NY!",
+                            2000   
+                        ]}
+                    />
+                </p>
+
             </div>
-           
+
         </div>
 
     )
