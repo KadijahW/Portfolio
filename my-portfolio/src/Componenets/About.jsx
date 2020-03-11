c23bb93b75c4389c036255a65044ea59923deecf
kadjiah <kadijahwilson@pursuit.org>
2020-03-10 21:59:32 -0400

    completed css for about page

--- my-portfolio/src/Componenets/About.jsx
+++ my-portfolio/src/Componenets/About.jsx
@@ -1,40 +1,58 @@
 import React from 'react'
-import {FaAdobe, FaGitAlt, FaReact} from 'react-icons/fa'
-import{DiPostgresql} from 'react-icons/di'
-import {IoLogoJavascript, IoLogoHtml5, IoLogoCss3} from 'react-icons/io'
+import { FaAdobe, FaGitAlt, FaReact } from 'react-icons/fa'
+import { DiPostgresql } from 'react-icons/di'
+import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from 'react-icons/io'
 import '../CSS/About.css';
 
 const About = () => {
-    return(
-<div className= 'main-container'>
-    <div className='firstSection'>
-<p>Random Text</p>
-    </div>
-    <div className='secSection'>
-    <h1>ABOUT</h1>
+    return (
+        <div className='main-container'>
 
-    </div>
-    <div className='thirdSection'>
-<h1>Some skills that I know</h1>
-    </div>
-    <div className='fourthSection'>
-        <IoLogoJavascript className='skill-icon'/>
-        <p>Javascript</p>
-        <IoLogoHtml5  className='skill-icon'/>
-        <p>HTML</p>
-        <IoLogoCss3  className='skill-icon'/>
-        <p>CSS</p>
-        <FaAdobe  className='skill-icon'/>
-        <p>Adobe</p>
-        <DiPostgresql  className='skill-icon'/>
-        <p>PostGres</p>
-        <FaGitAlt  className='skill-icon'/>
-        <p>GitHub</p>
-        <FaReact className='skill-icon' />
-        <p>React</p>
-    </div>
+            <div className='firstSection'>
+                <p>Random Text</p>
+            </div>
 
-</div>
+            <div className='secSection'>
+                <h1>Incase you wanted to read more about me</h1>
+            </div>
+
+            <div className='thirdSection'>
+                <h1>Some skills that I know</h1>
+            </div>
+
+            <div className='fourthSection'>
+                <div>
+                    <IoLogoJavascript className='skill-icon' style={{ color: "yellow" }} />
+                    <p>Javascript</p>
+                </div>
+                <div>
+                    <IoLogoHtml5 className='skill-icon' style={{ color: "orangered" }} />
+                    <p>HTML</p>
+                </div>
+                <div>
+                    <IoLogoCss3 className='skill-icon' style={{ color: "blue" }} />
+                    <p>CSS</p>
+                </div>
+                <div>
+                    <FaAdobe className='skill-icon' style={{ color: "red" }} />
+                    <p>Adobe</p>
+                </div>
+                <div>
+                    <DiPostgresql className='skill-icon' style={{ color: "darkblue" }} />
+                    <p>PostGres</p>
+                </div>
+                <div>
+                    <FaGitAlt className='skill-icon' style={{ color: "orange" }} />
+                    <p>GitHub</p>
+                </div>
+                <div>
+                    <FaReact className='skill-icon' />
+                    <p>React</p>
+                </div>
+
+            </div>
+
+        </div>
     );
 }
 
