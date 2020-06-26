8a77ad0d390e9ff908d2f4d0e37ca93f18062fd4
kadjiah <kadijahwilson@pursuit.org>
2020-06-26 15:07:58 -0400

    added projects to an array and mapped through the projects. cleaned up code

--- my-portfolio/src/Componenets/Projects.jsx
+++ my-portfolio/src/Componenets/Projects.jsx
@@ -1,78 +1,47 @@
 import React from 'react'
 import '../CSS/Projects.css'
+import ProjectCard from './ProjectCard'
 // import {Link} from 'react-router-dom'
+
+const projects = [
+{
+    title: "Youtube Replica", 
+    description: "This app is a replica of the website youtube I used the youtube API to fetch the data(videos) that the user is searching for. Users are allowed to watch the videos and leave comments.",
+    img: "images/youtube_replica.png",
+    repo: "https://github.com/KadijahW/Pursuit-Core-Web-Unit-4-Assessment"
+},
+{
+    title: "Shutter",
+    description: "Shutter is a social media web app that concentrates in sharing high quality snapshots of our users interests and experiences.",
+    img: "../images/Shutter.png",
+    repo: "https://github.com/KadijahW/Shutter"
+},
+{
+    title: "Simple 21",
+    description: "Developed a web based game app that allows users to play against the computer. The web app allows users to draw cards, hit or stay, and draws cards for the computer. Whoever scores 21 or is closer to 21 wins.",
+    img: "../images/Simple21.png",
+    repo: "https://github.com/KadijahW/21-web-game"
+},
+{
+    title: "Tekpack",
+    description: "TekPack is your one stop shop to manage and keep track of product specifications and its related content seamlessly, between both designers and manufacutures.",
+    img: "/images/Tekpack.png",
+    repo: "https://github.com/KadijahW/Tekpack"
+},
+{
+    title: "Remote Theater",
+    description: "Remote Theater is a web app that replicates a physical movie theater. Allows users to watch movies at a specified showtime and also has a chat feature that allows users to communicate while the movie is playing.",
+    img: "/images/Remote-Theater.png",
+    repo: "https://github.com/KadijahW/remote-theater"
+}
+
+]
 
 
 const Projects = () => {
     return (
         <div className='mainContainer'>
-            <div className='projects-container'>
-            <h1 className='title'>Youtube Replica</h1>
-                <div className='projectCard'>
-                    <div className='flip-card-front'>
-                        <img src={require('../images/youtube_replica.png')} alt="youtube screenshot" className='project-img' />
-                    </div>
-                    <div className='flip-card-back'>
-                        <p> This app is a replica of the website youtube.
-                        I used the youtube API to fetch the data(videos) that
-                        the user is searching for. Users are allowed to watch
-                        the videos and leave comments.
-                    </p>
-                    <a href='https://github.com/KadijahW/Pursuit-Core-Web-Unit-4-Assessment' target='_blank' rel="noopener noreferrer"> <button>Github</button></a>    
-                    </div>
-                </div>
-            </div>
-
-            <div className='projects-container'>
-            <h1 className='title'>Shutter</h1>
-                <div className='projectCard'>
-                    <div className='flip-card-front'>
-                        <img src={require('../images/Shutter.png')} alt="shutter screenshot" className='project-img' />
-                    </div>
-                    <div className='flip-card-back'>
-                        <p>
-                            Shutter is a social media web app that concentrates
-                            in sharing high quality snapshots of our users interests
-                            and experiences.
-                    </p>
-                    <a href='https://github.com/KadijahW/Shutter' target='_blank' rel="noopener noreferrer"> <button>Github</button></a>    
-                    </div>
-                </div>
-            </div>
-
-            <div className='projects-container'>
-            <h1 className='title'>Simple 21</h1>
-                <div className='projectCard'>
-                    <div className='flip-card-front'>
-                        <img src={require('../images/Simple21.png')} alt="shutter screenshot" className='project-img' />
-                    </div>
-                    <div className='flip-card-back'>
-                        <p>
-                            Developed a web based game app that allows users to play against the computer.
-                            The web app allows users to draw cards, hit or stay, and draws cards for the computer.
-                            Whoever scores 21 or is closer to 21 wins.
-                    </p>
-                        <a href='https://github.com/KadijahW/21-web-game' target='_blank' rel="noopener noreferrer"> <button>Github</button></a>    
-                    </div>
-                </div>
-            </div>
-
-            <div className='projects-container'>
-            <h1 className='title'>Tekpack</h1>
-                <div className='projectCard'>
-                    <div className='flip-card-front'>
-                        <img src={require('../images/Tekpack.png')} alt="tekpack screenshot" className='project-img' />
-                    </div>
-                    <div className='flip-card-back'>
-                        <p>
-                        TekPack is your one stop shop to manage and keep track of product specifications and its 
-                        related content seamlessly, between both designers and manufacutures.
-                    </p>
-                    <a href='https://github.com/KadijahW/Tekpack' target='_blank' rel="noopener noreferrer"> <button>Github</button></a>  
-                    </div>
-                </div>
-            </div>
-
+         {projects.map(p => <ProjectCard project={p} /> )}
         </div>
     )
 }
