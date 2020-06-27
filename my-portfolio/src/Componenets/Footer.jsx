13919a14480f8c9160680e24704416fcefcfcb81
kadjiah <kadijahwilson@pursuit.org>
2020-06-27 16:14:12 -0400

    adjusted footer for each page

--- my-portfolio/src/Componenets/Footer.jsx
+++ my-portfolio/src/Componenets/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className='fab'>
         <a href='https://github.com/KadijahW' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon className='icon' icon={['fab', 'github']} /></a>
         <a href='https://www.linkedin.com/in/kadijah-wilson-b33696191/' target='_blank' rel="noopener noreferrer" ><FontAwesomeIcon className='icon' icon={['fab', 'linkedin']} /> </a>
-        <a href="https://formview.io/#/jepsxkaowkkthhg/contactform?header=0&theme=readable"> <FontAwesomeIcon icon={faEnvelope} className='icon'/> </a>
+        <a href="https://jepsxkaowkkthhg.form.io/contactform"> <FontAwesomeIcon icon={faEnvelope} className='icon'/> </a>
 
     </div>
     )
