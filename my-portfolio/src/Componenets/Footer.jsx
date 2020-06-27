8a722c3c6c126fa360fe278d747250d302a61586
kadjiah <kadijahwilson@pursuit.org>
2020-06-27 17:55:52 -0400

    added mail to to the footer where users can contact me

--- my-portfolio/src/Componenets/Footer.jsx
+++ my-portfolio/src/Componenets/Footer.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+
 import '../CSS/Footer.css';
 
 
-const Footer = () => {
-    return(
-        <div className='fab'>
-        <a href='https://github.com/KadijahW' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon className='icon' icon={['fab', 'github']} /></a>
-        <a href='https://www.linkedin.com/in/kadijah-wilson-b33696191/' target='_blank' rel="noopener noreferrer" ><FontAwesomeIcon className='icon' icon={['fab', 'linkedin']} /> </a>
-        <a href="https://jepsxkaowkkthhg.form.io/contactform"> <FontAwesomeIcon icon={faEnvelope} className='icon'/> </a>
 
-    </div>
+
+const Footer = () => {
+    return (
+        <div className='fab'>
+            <a href='https://github.com/KadijahW' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon className='icon' icon={['fab', 'github']} /></a>
+            <a href='https://www.linkedin.com/in/kadijah-wilson-b33696191/' target='_blank' rel="noopener noreferrer" ><FontAwesomeIcon className='icon' icon={['fab', 'linkedin']} /> </a>
+            <a href="mailto:kadijahwilson@pursuit.org"><FontAwesomeIcon icon={faEnvelope} className='icon'/></a>
+
+{/* <>   
+<form action="https://formspree.io/xyyndpjk" method="POST">
+    <input type="text" placeholder="Name:" name="name"></input>
+    <input type="text" placeholder="Email:" name="_replyto"></input>
+    <input type="text" placeholder="Message" name="message"></input>
+    <input type="Submit" value="Send" ></input>
+</form>
+
+</> */}
+        </div>
+        
     )
 }
 
