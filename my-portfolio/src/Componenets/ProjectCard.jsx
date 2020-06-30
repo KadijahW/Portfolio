68268aa0c60746c952fc843681ce3fc5e67b6968
kadjiah <kadijahwilson@pursuit.org>
2020-06-29 21:54:05 -0400

    fixed back of project card to contain the description and links

--- my-portfolio/src/Componenets/ProjectCard.jsx
+++ my-portfolio/src/Componenets/ProjectCard.jsx
@@ -4,7 +4,7 @@ function ProjectCard({ project }) {
   const { title, description, img, repo, live } = project
   return (
       <div>
-    <h1>{title}</h1>
+    <h1 className="title">{title}</h1>
     <div className="projects-container">
       <div className="projectCard">
         <div className="flip-card-front">
@@ -13,8 +13,10 @@ function ProjectCard({ project }) {
 
         <div className="flip-card-back">
             <p>{description}</p>
-              <a href={repo}><p>GitHub</p></a>
-              <a href={live}><p>Live</p></a>
+            <span>
+              <a className="ref" href={repo}><p>GitHub</p></a> 
+              <a className="ref" href={live}><p>Live</p></a>
+              </span>
             </div>
           </div>
         </div>
