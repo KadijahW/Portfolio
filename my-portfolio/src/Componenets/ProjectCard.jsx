6a397df30f28da81618c207a65b3d7df471461ef
kadjiah <kadijahwilson@pursuit.org>
2020-07-03 12:15:41 -0400

    removed link to live site

--- my-portfolio/src/Componenets/ProjectCard.jsx
+++ my-portfolio/src/Componenets/ProjectCard.jsx
@@ -15,7 +15,7 @@ function ProjectCard({ project }) {
             <p>{description}</p>
             <span>
               <a className="ref" href={repo}><p>GitHub</p></a> 
-              <a className="ref" href={live}><p>Live</p></a>
+              {/* <a className="ref" href={live}><p>Live</p></a> */}
               </span>
             </div>
           </div>
