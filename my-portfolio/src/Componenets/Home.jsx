3fff47123731864a26cac91fe53ec244401f71e2
kadjiah <kadijahwilson@pursuit.org>
2020-08-02 16:21:25 -0400

    now able to use react typical and once typewriter is timedout words will be left on screen

--- my-portfolio/src/Componenets/Home.jsx
+++ my-portfolio/src/Componenets/Home.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typical from 'react-typical'
 import '../CSS/Home.css'
 
 const Home = () => {
+    let [displayTypical, setDisplayTypical] = useState(true)
+    setTimeout(() => {
+        setDisplayTypical(false)
+    }, 16000)
 
-return (
-        
-            <div className='intro'>
-                <> </>
+
+    return (
+        <div className='intro'>
+            {displayTypical ? (
                 <p>
                     <Typical
-                        loop={Infinity}
+                        loop={1}
                         wrapper="p"
                         steps={[
-                            "Welcome.",
+                            "Welcome!",
                             2000,
                             "My name is Kadijah.",
                             2000,
                             "I'm a Software Engineer.",
                             2000,
                             "Based in Brooklyn, NY!",
-                            2000   
+                            2000
                         ]}
                     />
                 </p>
+            ) : (
+                    <div className="displayIntro">
+                        <p>Welcome!</p>
+                        <p>My name is Kadijah.</p>
+                        <p> I'm a Software Engineer.</p>
+                        <p>Based in Brooklyn, NY!</p>
 
-            </div>
+                    </div>
+                )
+
+            }
+
+
+        </div>
 
 
     )
