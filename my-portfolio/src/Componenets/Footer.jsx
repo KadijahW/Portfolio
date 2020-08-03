50a4a9c46ae8caf9385126cb2edb03dbcea74c6b
kadjiah <kadijahwilson@pursuit.org>
2020-08-03 11:54:44 -0400

    fixed linkedIn URL

--- my-portfolio/src/Componenets/Footer.jsx
+++ my-portfolio/src/Componenets/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return (
         <div className='fab'>
             <a href='https://github.com/KadijahW' target='_blank' rel="noopener noreferrer"><FontAwesomeIcon className='icon' icon={['fab', 'github']} /></a>
-            <a href='https://www.linkedin.com/in/kadijah-wilson-b33696191/' target='_blank' rel="noopener noreferrer" ><FontAwesomeIcon className='icon' icon={['fab', 'linkedin']} /> </a>
+            <a href='https://www.linkedin.com/in/kadijah-wilson/' target='_blank' rel="noopener noreferrer" ><FontAwesomeIcon className='icon' icon={['fab', 'linkedin']} /> </a>
             <a href="mailto:kadijahwilson@pursuit.org"><FontAwesomeIcon icon={faEnvelope} className='icon'/></a>
 
 {/* <>   
