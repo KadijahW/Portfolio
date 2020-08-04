f77967a6bc9718578fa3e0390e3e23976cdb83e2
kadjiah <kadijahwilson@pursuit.org>
2020-08-04 18:21:14 -0400

    fixed issue with opening Resume link

--- my-portfolio/src/Componenets/Burger-Menu/RightNav.jsx
+++ my-portfolio/src/Componenets/Burger-Menu/RightNav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const StyledLink = styled(Link)
+const StyledLink = styled.div
     `
 .links{
     display: flex;
